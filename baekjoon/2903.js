8a2b93d5169c220919fbcd664e75100bffbaa407
Minchaez <rlawlsdl0730@gmail.com>
2026-02-22 21:42:05 +0900

    2.22 bronze3

---
+++ baekjoon/2903.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const input = require("fs").readFileSync(filePath).toString().trim();
+
+function solution() {
+  // 점 공식
+  // (2^n + 1)^2
+  const n = Number(input);
+  const result = (2 ** n + 1) ** 2;
+  return result;
+}
+console.log(solution());
