11d5cf6f7ab00b155b4b62c3ef077275cef5e07a
Minchaez <rlawlsdl0730@gmail.com>
2026-03-30 16:42:13 +0900

    3.30 silver 4

---
+++ baekjoon/10773.js
@@ -0,0 +1,24 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = Number(input[0]);
+const stack = [];
+
+function solution() {
+  for (let i = 1; i <= N; i++) {
+    const cmd = Number(input[i]);
+    if (cmd === 0) {
+      stack.pop();
+    } else {
+      stack.push(cmd);
+    }
+  }
+  let ans = stack.reduce((a, b) => a + b, 0);
+  console.log(ans);
+}
+
+solution();
